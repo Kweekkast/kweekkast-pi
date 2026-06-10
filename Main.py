9a169b9439d2df65204c972ecb590c416b626a85
Bownuz <fr.snelder@student.avans.nl>
2026-06-10 15:47:24 +0200

    Added test code

---
+++ Main.py
@@ -0,0 +1,10 @@
+class Main:
+    def __init__(self):
+        print("[MAIN] Ini\n")
+
+    def run(self) -> None:
+        print("[MAIN] Running\n")
+
+
+if __name__ == "__main__":
+    Main().run()
