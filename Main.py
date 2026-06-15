e116f0c2029a40fd4644ef82eaa720dc271a5293
davidVeer <davidvdveer05@gmail.com>
2026-06-15 12:06:14 +0200

    Completing merge in order to make sure everything works propperly

--- Main.py
+++ Main.py
@@ -1,22 +1,20 @@
-from ImageCapturing.CameraComponentHandler import CameraComponentHandler
-from timeit import Timer
 from threading import Thread
 from ImageCapturing.CameraComponentHandler import CameraComponentHandler
 from ImageCapturing.Triggers.TimerTrigger import TimerTrigger
 from ImageCapturing.Triggers.Buttontrigger import ButtonTrigger
 from ImageCapturing.Actioners.ImageCapturer import ImageCapturer
 
-cameraComponentHandler = CameraComponentHandler(TimerTrigger)
-cameraComponentHandler.run()
+#cameraComponentHandler = CameraComponentHandler(TimerTrigger)
+#cameraComponentHandler.run()
 
 trigger = TimerTrigger(60) #TimerTrigger(60) for timer trigger, ButtonTrigger() for button trigger
 thread = Thread(target =trigger.triggerLoop, args = ())
 thread.start()
 imageCapturer1 = ImageCapturer(0)
 trigger.AddObserver(imageCapturer1)
-imageCapturer2 = ImageCapturer(1)
-trigger.AddObserver(imageCapturer2)
-imageCapturer3 = ImageCapturer(2)
-trigger.AddObserver(imageCapturer3)
+#imageCapturer2 = ImageCapturer(1)
+#trigger.AddObserver(imageCapturer2)
+#imageCapturer3 = ImageCapturer(2)
+#trigger.AddObserver(imageCapturer3)
 
 
