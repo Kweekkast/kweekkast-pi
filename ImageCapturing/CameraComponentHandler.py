5c239b9d1e03a237f26cfb628ce2b011d9ea02af
davidVeer <davidvdveer05@gmail.com>
2026-06-15 12:13:34 +0200

    slightly refining CameraComponentHandler

--- ImageCapturing/CameraComponentHandler.py
+++ ImageCapturing/CameraComponentHandler.py
@@ -1,17 +1,17 @@
+from threading import Thread
 from ImageCapturing.Actioners.ImageCapturer import ImageCapturer
 
 class CameraComponentHandler():
-    def __init__(self, abstractTrigger):
+    def __init__(self, abstractTrigger, cameras):
         self.trigger = abstractTrigger
 
-        imageCapturer1 = ImageCapturer(0)
-        self.trigger.AddObserver(imageCapturer1)
-        imageCapturer2 = ImageCapturer(1)
-        self.trigger.AddObserver(imageCapturer2)
-        imageCapturer3 = ImageCapturer(2)
-        self.trigger.AddObserver(imageCapturer3)
+        for x in range(cameras):
+            imageCapturer = ImageCapturer(x)
+            self.trigger.AddObserver(imageCapturer)
 
 
 
     def run(self):
-        self.trigger.triggerLoop()
+        thread = Thread(target =self.trigger.triggerLoop(), args = ())
+        thread.start()
+        
--- Main.py
+++ Main.py
@@ -1,10 +1,9 @@
-from threading import Thread
 from ImageCapturing.CameraComponentHandler import CameraComponentHandler
 from ImageCapturing.Triggers.TimerTrigger import TimerTrigger
 
-cameraComponentHandler = CameraComponentHandler(TimerTrigger(60))
-thread = Thread(target =cameraComponentHandler.run(), args = ())
-thread.start()
+cameraComponentHandler = CameraComponentHandler(TimerTrigger(60),1)
+cameraComponentHandler.run()
+
 
 
 
