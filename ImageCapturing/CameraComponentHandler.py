44bd95822f450880e67da0d9f8433508ca7dba26
davidVeer <davidvdveer05@gmail.com>
2026-06-15 14:47:42 +0200

    Adding ImageCaptureRelease functionality

--- ImageCapturing/CameraComponentHandler.py
+++ ImageCapturing/CameraComponentHandler.py
@@ -4,14 +4,24 @@ from ImageCapturing.Actioners.ImageCapturer import ImageCapturer
 class CameraComponentHandler():
     def __init__(self, abstractTrigger, cameras):
         self.trigger = abstractTrigger
+        self.imageCapturers = []
 
         for x in range(cameras):
             imageCapturer = ImageCapturer(x)
             self.trigger.AddObserver(imageCapturer)
+            self.imageCapturers.append(imageCapturer)
 
 
 
     def run(self):
         thread = Thread(target =self.trigger.triggerLoop(), args = ())
         thread.start()
+        time.sleep(300)
+        self.ReleaseAllCameras()
+
+
+    def ReleaseAllCameras(self):
+        for imageCapturer in self.imageCapturers:
+            self.trigger.removeObserver(imageCapturer)
+            imageCapturer.__del__()
         
--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 from ImageCapturing.CameraComponentHandler import CameraComponentHandler
 from ImageCapturing.Triggers.TimerTrigger import TimerTrigger
 
-cameraComponentHandler = CameraComponentHandler(TimerTrigger(60),1)
+cameraComponentHandler = CameraComponentHandler(TimerTrigger(30),1)
 cameraComponentHandler.run()
 
 
