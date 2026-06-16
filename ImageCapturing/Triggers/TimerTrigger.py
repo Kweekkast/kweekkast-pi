4a69154a5cc274796074e3b6a28f93b170f95bd3
davidVeer <davidvdveer05@gmail.com>
2026-06-16 20:51:20 +0200

    Adding logs to timertrigger

--- ImageCapturing/Triggers/TimerTrigger.py
+++ ImageCapturing/Triggers/TimerTrigger.py
@@ -18,6 +18,12 @@ class TimerTrigger(Abstract_Trigger):
 
             timeEnd = time.time()
             timeElapsed = timeEnd - timeBegin
-            if timeElapsed > self.timeSeconds:
-                timeElapsed = self.timeSeconds
-            time.sleep(self.timeSeconds-timeElapsed)
+            recalculatedSleeptime = self.timeSeconds-timeElapsed
+
+            if recalculatedSleeptime < 0:
+                recalculatedSleeptime = 0
+
+            FileLogger.logger.Log(MessageSeverity.DEV, __class__.__name__, "notifying all observers took: " + str(timeElapsed) + " Seconds")
+            FileLogger.logger.Log(MessageSeverity.DEV, __class__.__name__, "Sleeping " + str(recalculatedSleeptime) + " Seconds before renotifying observers")
+            
+            time.sleep(recalculatedSleeptime)
