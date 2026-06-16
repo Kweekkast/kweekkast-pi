6046a2ba5166cca2d655b04d72e6d5b2cd43f649
davidVeer <davidvdveer05@gmail.com>
2026-06-16 13:24:35 +0200

    added string formatters (string formatter should be fixed later))

--- StringFormatter.py
+++ StringFormatter.py
@@ -4,17 +4,31 @@ def createImagePath(ImageCaptureDevice):
     imagePath = "Assets/Device_" + str(ImageCaptureDevice) + "/" + fetchCustomDateString() + ".png"
     return imagePath
 
-def fetchCustomDateString():
+def fetchCustomDateTimeString():
     time_Format = "%Y-%m-%d %H-%M-%S-%f"
     
     date = datetime.datetime.now()
     customDatetimeString = str(date.strftime(time_Format[:-3]))
     return str(customDatetimeString)
 
+def fetchCustomDateString():
+    time_Format = "%Y-%m-%d"
+    
+    date = datetime.datetime.now()
+    customDatetimeString = str(date.strftime(time_Format[:-3]))
+    return str(customDatetimeString)
+
+def fetchCustomTimeString():
+    time_Format = "%H-%M-%S-%f"
+    
+    date = datetime.datetime.now()
+    customDatetimeString = str(date.strftime(time_Format[:-3]))
+    return str(customDatetimeString)
+
 def FormatLogMessage(logSeverity, senderClass, msg):
-        timestamp = fetchCustomDateString()
+        timestamp = fetchCustomDateTimeString()
         return f"{timestamp}: {logSeverity.name}: {senderClass}: {msg}"
 
-SessionLogDirectoryPath = "logs/"
-SessionLogfileName = "log_session_" + fetchCustomDateString() + ".txt"
+SessionLogDirectoryPath = "logs/" + fetchCustomDateString() + "/"
+SessionLogfileName = "log_session_" + fetchCustomTimeString() + ".txt"
 SessionLogFilePath = SessionLogDirectoryPath + SessionLogfileName
