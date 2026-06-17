2b5fd382f294e9acddcc3b95efb49c03d28897a2
Bownuz <fr.snelder@student.avans.nl>
2026-06-17 18:38:33 +0200

    Logger toegevoegt woordt main net pi

--- net/src/kweekkast_net/main.py
+++ net/src/kweekkast_net/main.py
@@ -1,6 +1,7 @@
 def main() -> None:
     print("[kweekkast-net] Nog geen netwerkapplicatie geimplementeerd.")
-
+    file_logger.logger.log(MessageSeverity.INFO, self.__class__.__name__,
+                           f"Nog geen netwerkapplicatie geimplementeerd")
 
 if __name__ == "__main__":
     main()
