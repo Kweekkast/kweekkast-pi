edc1f8bd979cafcc193bff3184d296abe346f234
davidVeer <davidvdveer05@gmail.com>
2026-06-17 10:54:51 +0200

    fixed a missed function  name

--- ImageCapturing/camera_component_handler.py
+++ ImageCapturing/camera_component_handler.py
@@ -10,16 +10,16 @@ class CameraComponentHandler():
 
         for x in range(cameras):
             imageCapturer = ImageCapturer(x)
-            self._TRIGGER.AddObserver(imageCapturer)
+            self._TRIGGER.add_observer(imageCapturer)
             self._IMAGE_CAPTURERS.append(imageCapturer)
             FileLogger.logger.Log(MessageSeverity.DEV, __class__.__name__,"an imageCapturer was added to the list")
 
     def run(self):
-        thread = Thread(target =self._TRIGGER.triggerLoop, args = ())
+        thread = Thread(target =self._TRIGGER.trigger_loop, args = ())
         thread.start()
 
     def release_all_cameras(self):
         for image_capturer in self._IMAGE_CAPTURERS:
-            self._TRIGGER.removeObserver(image_capturer)
+            self._TRIGGER.remove_observer(image_capturer)
             image_capturer.__del__()
             FileLogger.logger.Log(MessageSeverity.DEV, __class__.__name__,"an imageCapturer was released" )
