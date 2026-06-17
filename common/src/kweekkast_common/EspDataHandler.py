3c1a18194fb3af829138f12ee35949f4e4c6b869
Max <52867741+SpaceYoshi@users.noreply.github.com>
2026-06-17 20:18:55 +0200

    Distributor not Distributer

--- common/src/kweekkast_common/EspDataHandler.py
+++ common/src/kweekkast_common/EspDataHandler.py
@@ -1,6 +1,5 @@
-from __future__ import annotations
-
 from kweekkast_common.communication_component.Subscriber import Subscriber
+from kweekkast_common.communication_component.communicator_distributor import CommunicatorDistributor
 from kweekkast_common.logger_component import file_logger
 from kweekkast_common.logger_component.logger_enum import MessageSeverity
 from kweekkast_common.reading import Reading
@@ -11,11 +10,11 @@ class EspDataHandler(Subscriber):
     Ontvangt data van ESP-communicators en stuurt deze door naar de Pi-communicator.
     """
 
-    def __init__(self, distributer: "CommunicatorDistributer"):
-        self.distributer = distributer
+    def __init__(self, distributor: "CommunicatorDistributor"):
+        self.distributor = distributor
 
     def Notify(self, reading: Reading) -> None:
-        piCommunicator = self.distributer.FindPiCommunicator()
+        piCommunicator = self.distributor.FindPiCommunicator()
         if piCommunicator and piCommunicator.director:
             file_logger.logger.log(MessageSeverity.INFO, self.__class__.__name__,
                                    f"Doorsturen naar Pi: {reading.message}")
